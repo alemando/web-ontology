a870f2cff873ce7f73569cda28b3ae126a55d93a
Juan Mejia <juanmejiadev@gmail.com>
2022-08-09 19:24:22 -0500

    Tarea 1

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from dataclasses import Field
+import tempfile
+import shutil
+from rdflib import Graph
+
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/")
+##Models 
 
-def home():
-    return {"Hello": "World"}
+# class Ontology(BaseModel):
+#     direction: str = Field(...,
+#                            example= 'archivo.ttl')
+
+g=Graph()
+
+@app.post("/post")
+
+def postOntology(
+    onto = Body(...)
+):
+    g.parse(onto)
+    tempdir = tempfile.mkdtemp(prefix="myapplication-")
+    return g.serialize(format='json-ld')
+    
+@app.get("/read", status_code=201)
+
+def readOntology():
+    return g
+
+
+
+
+
