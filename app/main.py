70e0042322214e648df4f5a0cf98903c631f907f
juanme21 <106550509+juanme21@users.noreply.github.com>
2022-10-15 16:20:44 -0500

    Sparql applied
    Changed the properties counting method from loops to Sparql queries.
    Also added the pizza TTL ontology to testing.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from rdflib import Graph
+from rdflib import Graph, BNode
 
 from fastapi import FastAPI
 from fastapi import FastAPI, File, HTTPException
@@ -27,4 +27,98 @@ async def root():
     if some:
         return {"msg": "Hello World"}
     else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get(
+    "/metrics/",
+    responses={
+        404: {"description": "Item not found"},
+    },
+)
+async def root():
+    try:
+
+        metrics = {
+            "Class": 0,
+            "ObjectProperty": 0,
+            "DataProperty": 0,
+            "Individual": 0,
+            "Subclass": 0,
+            "DisjointClasses": 0,
+            "EquivalentClass": 0,
+            "SubObjectProperty": 0,
+            "Inverse": 0,
+            "FunctionalProperty": 0,
+            "TransitiveProperty": 0,
+            "AsymmetricProperty": 0,
+            "SymmetricProperty": 0,
+            "IrreflexiveProperty": 0,
+            "ReflexiveProperty": 0,
+        }
+
+        ## object querys
+        objectBaseConsult = """
+        PREFIX : <http://www.pizzaowl.org#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+        SELECT DISTINCT ?pizza WHERE { 
+        ?pizza rdf:type %s
+        }
+        """
+
+        classConsult = (
+            objectBaseConsult
+            % "owl:Class \n FILTER NOT EXISTS {?pizza owl:equivalentClass ?nodo}"
+        )
+        objectPropertyConsult = objectBaseConsult % "owl:ObjectProperty"
+        individualConsult = objectBaseConsult % "owl:NamedIndividual"
+        dataPropertyConsult = objectBaseConsult % "owl:DatatypeProperty"
+        # disjointClassConsult = objectBaseConsult % "owl:AllDisjointClasses"
+
+        ## predicate querys
+        predicateBaseConsult = """
+        PREFIX : <http://www.pizzaowl.org#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+        SELECT ?pizza WHERE { 
+        ?pizza %s ?something
+        }
+        """
+        subClassPropertyConsult = predicateBaseConsult % "rdfs:subClassOf"
+        equivalentPropertyConsult = predicateBaseConsult % "owl:equivalentClass"
+        disjointPropertyConsult = objectBaseConsult % "owl:AllDisjointClasses"
+        subPropertyConsult = predicateBaseConsult % "rdfs:subPropertyOf"
+        functionalPropertyConsult = predicateBaseConsult % "owl:FunctionalProperty"
+        inversePropertyConsult = predicateBaseConsult % "owl:inverseOf"
+        transitivePropertyConsult = predicateBaseConsult % "owl:TransitiveProperty"
+        asymmetricPropertyConsult = predicateBaseConsult % "owl:AsymmetricProperty"
+        symmetricPropertyClassConsult = predicateBaseConsult % "owl:SymmetricProperty"
+        irreflexivePropertyConsult = predicateBaseConsult % "owl:IrreflexiveProperty"
+        reflexivePropertyConsult = predicateBaseConsult % "owl:ReflexiveProperty"
+
+        metricsAcces = {
+            "Class": classConsult,
+            "Individual": individualConsult,
+            "DataProperty": dataPropertyConsult,
+            "ObjectProperty": objectPropertyConsult,
+            "Subclass": subClassPropertyConsult,
+            "EquivalentClass": equivalentPropertyConsult,
+            "SubObjectProperty": subPropertyConsult,
+            "FunctionalProperty": functionalPropertyConsult,
+            "Inverse": inversePropertyConsult,
+            "TransitiveProperty": transitivePropertyConsult,
+            "AsymmetricProperty": asymmetricPropertyConsult,
+            "SymmetricProperty": symmetricPropertyClassConsult,
+            "ReflexiveProperty": reflexivePropertyConsult,
+            "IrreflexiveProperty": irreflexivePropertyConsult,
+            "DisjointClasses": disjointPropertyConsult,
+        }
+
+        for k in metricsAcces:
+            metrics[k] += len(list(g.query(metricsAcces[k])))
+
+        return metrics
+    except:
         raise HTTPException(status_code=404, detail="Item not found")
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -12,6 +12,32 @@ def test_read_main():
     assert response.json() == {"msg": "Hello World"}
 
 
+def test_read_metrics():
+    client.post(
+        "/files/",
+        files={"file": open(testFilesDirectory + "pizza.ttl", "rb")},
+    )
+    response = client.get("/metrics/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Class": 42,
+        "ObjectProperty": 7,
+        "DataProperty": 1,
+        "Individual": 9,
+        "Subclass": 55,
+        "DisjointClasses": 7,
+        "EquivalentClass": 7,
+        "SubObjectProperty": 4,
+        "Inverse": 2,
+        "FunctionalProperty": 0,
+        "TransitiveProperty": 0,
+        "AsymmetricProperty": 0,
+        "SymmetricProperty": 0,
+        "IrreflexiveProperty": 0,
+        "ReflexiveProperty": 0,
+    }
+
+
 def test_read_file():
     response = client.post(
         "/files/",
